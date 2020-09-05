962e6d28d1419d0cde6b6c55dac9ee31059e35d1
shilpa <shilpa07udupi@gmail.com>
2020-09-05 12:00:33 +0530

    storing in dictionary

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -4,20 +4,32 @@ read -p " read input 1 " a
 read -p " read input 2 " b
 read -p " read input 3 " c
 
+declare -A dictionary
+i=0
+
 echo computing a+b*c
 result=$(($a+$b*$c))
 echo $result
+dictionary[$i]=$result
+i=$(($i+1)) 
 
 echo computing a*b+c
 result=$(($a*$b+$c))
 echo $result
+dictionary[$i]=$result
+i=$(($i+1))
 
 echo computing c+a/b
 result=$(($c+$a/$b))
 echo $result
+dictionary[$i]=$result
+i=$(($i+1))
 
 echo computing a%b+c
 result=$(($a%$b+$c))
 echo $result
+dictionary[$i]=$result
+i=$(($i+1))
 
-
+echo storing in dictionary
+echo ${dictionary[@]}
