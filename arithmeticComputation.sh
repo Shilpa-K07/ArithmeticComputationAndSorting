3d535e2752355e8f25ec748d8c82eafed5812b8a
shilpa <shilpa07udupi@gmail.com>
2020-09-05 11:51:12 +0530

    adding previous commit

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -4,6 +4,33 @@ read -p " read input 1 " a
 read -p " read input 2 " b
 read -p " read input 3 " c
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 echo computing a+b*c
 result=$(($a+$b*$c))
 echo $result
