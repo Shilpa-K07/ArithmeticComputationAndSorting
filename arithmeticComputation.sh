0694e01dcfe9aec4ae10b366fc85331cbac552fc
shilpa <shilpa07udupi@gmail.com>
2020-09-05 09:02:04 +0530

    reading inputs

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,5 +1,5 @@
 echo Welcome to Arithmetic COmputation and Sorting !!
 
-read -p " read input 1 " ip1
-read -p " read input 2 " ip2
-read -p " read input 3 " ip3
+read -p " read input 1 " a
+read -p " read input 2 " b
+read -p " read input 3 " c
