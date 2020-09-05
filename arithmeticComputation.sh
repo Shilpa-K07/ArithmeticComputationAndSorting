ad795d06a546e40a3bcd6aa57f5d13d8b3869cf4
shilpa <shilpa07udupi@gmail.com>
2020-09-05 09:50:39 +0530

    computing a*b+c

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -4,6 +4,10 @@ read -p " read input 1 " a
 read -p " read input 2 " b
 read -p " read input 3 " c
 
+echo computing a+b*c
+result=$(($a+$b*$c))
+echo $result
+
 echo computing a*b+c
 result=$(($a*$b+$c))
 echo $result
